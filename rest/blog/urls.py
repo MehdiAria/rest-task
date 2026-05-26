3ff9f8ea55b40f354c32d5a5b8d01eb3dc2ea7ef
Leviathan <mahdishahidi010@gmail.com>
2026-05-27 01:58:23 +0330

    Delete rest directory

--- rest/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import PostsListAPIView, RateCreateAPIView
-
-urlpatterns = [
-    path('api/posts', PostsListAPIView.as_view(), name='posts-list-api'),  # API endpoint for list of posts
-    path('api/posts/rate', RateCreateAPIView.as_view(), name='rate-api'),  # API endpoint for creating rate
-]
